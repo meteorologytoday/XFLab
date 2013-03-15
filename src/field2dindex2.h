6b834294d0929ee89bbb2435c4da21bb49f92ca8
Xtt <meteorologytoday@gmail.com>
2013-03-15 18:25:48 +0800

    Remove the Grid class, use Matrix for simplicity. Redefine the concept of field inheritance :      Field            -> just a field      Field2D          -> a field with 2 dimensional space      Field2DIndex2    -> a 2D field with 2 indices operator(i,j)
    Add enumator : Enumerator1, Enumerator2

--- src/field2dindex2.h
+++ src/field2dindex2.h
@@ -1,17 +1,17 @@
 #include "typedef.h"
 #include "field2d.h"
 
-#ifndef X_FIELD_2D_MESHGRID_H
-#define X_FIELD_2D_MESHGRID_H
+#ifndef X_FIELD_2D_INDEX_2_H
+#define X_FIELD_2D_INDEX_2_H
 
 namespace X {
 
     template <typename T, xsize P1, xsize P2, xsize N> 
-        class Field2DMeshgrid : public Field2D<T,P1*P2,N> {
+        class Field2DIndex2 : public Field2D<T,P1*P2,N> {
             private:
             public:
-                Field2DMeshgrid();
-                ~Field2DMeshgrid();
+                Field2DIndex2();
+                ~Field2DIndex2();
 
                 inline T& operator()(xsize n, xsize i, xsize j) {
                     return (this->operator[](n))(OFFSET2(P1,P2,i,j));
