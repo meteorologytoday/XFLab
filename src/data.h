3556644919c4482ef1a7a80d91b6e271fe2a09e9
meteorologytoday <meteorologytoday@gmail.com>
2013-09-02 22:30:44 +0800

    Testing

--- src/data.h
+++ src/data.h
@@ -2,20 +2,42 @@
 #ifndef X_DATA_H
 #define X_DATA_H
 namespace X {
+
+
+    /*! \class Data data.h
+     *  \author Hsu, Tien-Yiao
+     *  \copyright NTUAS DMLab
+     *  \date 2013/04/27
+     *  \version 1.0
+     *  
+     *  \brief This class is the base class for all the data.
+     *  \details Data needs to be specified its data type and number of elements. #data
+     */
     template <typename T, xsize N>
         class Data {
             private:
-                T *data;
-                bool manage_memory;
-            public:
+                T *data; /*!< The heap memory where #data are stored. */
+            public: 
+                /*! 
+                 * Initialize an array of N elements with type T.
+                 * They are all with default initialized value.
+                 */
                 Data();
-                Data(T*);      // initialize by an pre-defined array
+                
+                /*! 
+                 * Deconstructor frees all the heap memory of the object.
+                 */
                 ~Data();
 
-                inline T* getDataPointer() { return this->data; }
-                inline T* p() { return this->getDataPointer(); }
-                inline T& getData(xsize i) { return this->data[i]; }
-                inline xsize getSize() { return this->N; }
+                inline T* getDataPointer() { return this->data; }   /*!< Return the pointer of the data */
+                inline void setDataPointer(T* data) { this->data = data; }   /*!< Return the pointer of the data */
+                inline T* p() { return this->getDataPointer(); }    /*!< Abbreviation for getDataPointer() */
+                inline void setp(T* data) { this->setDataPointer(data); }   /*!< Return the pointer of the data */
+                inline T& getData(xsize i) { return this->data[i]; }/*!< Get the i-th element of the data. */
+                
+                /*!
+                 *  Copies the value of another data.
+                 */
                 inline void copyData(Data<T,N>& data) { for(xsize i=0; i < N; ++i) (this->getData(i)) = data.getData(i); }
 
             public:
--- src/gnuplot_magic.cpp
+++ src/gnuplot_magic.cpp
@@ -1,41 +1,64 @@
-
+#include <stdio.h>
 #ifdef X_GNUPLOT_MAGIC_H
 #ifndef X_GNUPLOT_MAGIC_CPP
 #define X_GNUPLOT_MAGIC_CPP
 
 namespace X {
     namespace GNUPLOT {
-        template <typename T>
-            void output_field(const char *filename, T *field, T *x, T *y, xsize n0, xsize n1) {
-                
-                ofstream myFile;
-                float* row = new float[n0+1]();
+	template <typename T>
+	    void output_field(const char *filename, T *field, T *x, T *y, xsize n0, xsize n1) {
 
-                myFile.open (filename, ios::out | ios::binary);
+		ofstream myFile;
+		float* row = new float[n0+1]();
 
-                // first line
-                for(xsize i = 0; i < (n0+1); ++i) {
-                    if(i==0) 
-                        row[i] = (float) n0; 
-                    else
-                        row[i] = (float) x[i-1];
-                }
+		myFile.open (filename, ios::out | ios::binary);
 
-                myFile.write((char *)row, (n0+1) * sizeof(float));
-                // rest
+		// first line
+		for(xsize i = 0; i < (n0+1); ++i) {
+		    if(i==0) 
+			row[i] = (float) n0; 
+		    else
+			row[i] = (float) x[i-1];
+		}
 
-                for(xsize j=0; j < n1; ++j) {
-                    for(xsize i=0; i < (n0+1); ++i) {
-                        if(i==0) 
-                            row[i] = (float) y[j];
-                        else 
-                            row[i] = (float) field[X::OFFSET2(n0,n1,i-1,j)]; 
-                    }
-                    myFile.write((char *)row, (n0+1) * sizeof(float));
-                }
-                myFile.close();
-                delete [] row;
-            }
+		myFile.write((char *)row, (n0+1) * sizeof(float));
+		// rest
+
+		for(xsize j=0; j < n1; ++j) {
+		    for(xsize i=0; i < (n0+1); ++i) {
+			if(i==0) 
+			    row[i] = (float) y[j];
+			else 
+			    row[i] = (float) field[X::OFFSET2(n0,n1,i-1,j)]; 
+		    }
+		    myFile.write((char *)row, (n0+1) * sizeof(float));
+		}
+		myFile.close();
+		delete [] row;
+	    }
+
+
+
+	template <typename T>
+	    void output_field_record(const char *filename, T *field, xsize n0, xsize n1) {
+
+		ofstream myFile;
+		float* row = new float[n1]();
+
+		myFile.open (filename, ios::out | ios::binary);
+
+		for(xsize i=0; i < n0; ++i) {
+		    for(xsize j=0; j < n1; ++j) {
+			row[j] = (float) field[X::OFFSET2(n0,n1,i,j)]; 
+		    }
+		    myFile.write((char *)row, n1 * sizeof(float));
+		}
+		myFile.close();
+		delete [] row;
+
+		
+
+	    }
 
     }
 
